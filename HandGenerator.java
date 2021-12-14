5a6b26afaada106f2e12bb24df188fa6a3293832
Lafalasidosi <lafalasidosi@pm.me>
2021-12-13 22:53:47 -0800

    generates 8 lines; BBO accepts

--- HandGenerator.java
+++ HandGenerator.java
@@ -4,16 +4,20 @@ public class HandGenerator {
     public static void main(String[] args){
         Deck myDeck = new Deck();
         Player[] table = {new Player("North"), new Player("East"), new Player("South"), new Player("West")};
-        myDeck.dealCards(table);
+        char[] strangeColumn = {'0', 'n', 'e', 'b', 'n', 'e', 'b', '0'};
 
-        for(Player p: table){
-            p.sortHand();
-        }
-       
         // want: open a file for writing
         // write 8 lines of LIN
         try (Formatter output = new Formatter("newLINfile.lin")) {
-            output.format("qx|o1|md|3%s,%s,%s|rh||ah|Board 1|sv|0|pg||", table[0].handToString(), table[1].handToString(), table[2].handToString());
+
+            for(int i = 0; i < 8; i++){
+                myDeck.dealCards(table);
+                for(Player p: table){
+                    p.sortHand();
+                }
+                output.format("qx|o%s|md|3%s,%s,%s|rh||ah|Board %s|sv|%s|pg||%n", i+1, table[0].handToString(), table[1].handToString(), table[2].handToString(), i+1, strangeColumn[i]);
+                myDeck.returnCardsToDeck(table);
+            }
         } catch (Exception e) {
             //TODO: handle exception
             System.err.println("You done goofed");
